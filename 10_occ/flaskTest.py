419e2ea9bf17bd7c46d0e17222b9190749604925
KennethChin0 <kchin00@stuy.edu>
2019-09-23 14:50:57 -0400

    first commit

--- 10_occ/flaskTest.py
+++ 10_occ/flaskTest.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__) #create instance of class FLASK
-#
+#ADDD HEADING
 @app.route("/") #assign following fxn to run when rotting route requested
 def hello_world():
     print ("HI")#runs in terminal when page is reloaded
@@ -13,6 +13,14 @@ def test_tmplt():
     coll = {0,1,1,2,3,5,8}
     return render_template(
     'foo.html',
+    collection = coll
+    )
+
+@app.route("/occpuyflaskst")
+def occpuyflaskst():
+    coll = {0,1,1,2,3,5,8}
+    return render_template(
+    'occupations.html',
     collection = coll
     )
 
