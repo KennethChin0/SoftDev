5ed920646d03dee58a2500a03e0042cc1701c595
Kenneth Chin <kchin00@stuy.edu>
2019-09-23 20:59:29 -0400

    inital commit

--- 10_occ/flaskTest.py
+++ 10_occ/flaskTest.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__) #create instance of class FLASK
 #ADDD HEADING
 @app.route("/") #assign following fxn to run when rotting route requested
@@ -8,20 +9,47 @@ def hello_world():
     file = open("index.html" , "r")
     return file.read()
 
-@app.route("/my_foist_template")
-def test_tmplt():
-    coll = {0,1,1,2,3,5,8}
-    return render_template(
-    'foo.html',
-    collection = coll
-    )
+# @app.route("/my_foist_template")
+# def test_tmplt():
+#     coll = {0,1,1,2,3,5,8}
+#     return render_template(
+#     'foo.html',
+#     collection = coll
+#     )
 
-@app.route("/occpuyflaskst")
+def MakeArray(fileName):
+  array = []
+  file = open(fileName,'r')
+  file.readline()
+  for line in file:
+      if '"' in line:
+          array.append(line.split('"')[1:])
+      else:
+          array.append(line.split(','))
+  array = array[:-1]
+  for line in array:
+      line[1] = line[1][:-1]
+      if ',' in line[1]:
+          line[1] = line[1][1:]
+  return array
+
+def MakeDict(array):
+  dic = {}
+  x = round(random.uniform(0,99.8),1)
+  y = 0
+  for line in array:
+      dic[line[0]] = line[1]
+      y += float(line[1])
+      if y > x:
+          return(line[0])
+
+@app.route("/occupyflaskst")
 def occpuyflaskst():
-    coll = {0,1,1,2,3,5,8}
+    coll = MakeArray("occupations.csv")
+    # coll = {0,1,1,2,3,5,8}
     return render_template(
     'occupations.html',
-    collection = coll
+    collection = coll, job = MakeDict(coll)
     )
 
 
