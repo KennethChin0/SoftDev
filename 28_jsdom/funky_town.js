94d76116676a1e5bdc847fbe9789a67c08f49faf
KennethChin0 <kchin00@stuy.edu>
2019-12-11 21:14:53 -0500

    finish

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -1,7 +1,4 @@
 
-var foo = function(n){
-  return n;
-};
 
 var fibonacci = function(n){
   if (n == 0) return 0;
@@ -25,4 +22,30 @@ var studentslist = ["Kenneth","Winston","Jeff","Nichol","Mr. Mykolyk"]
 var randomStudent = function(){
   var rand = Math.floor(Math.random() * studentslist.length);
   return studentslist[rand];
+}
+
+/*
+Team postfix --  Kevin Cai, Kenneth Chin
+SoftDev1 pd9
+K28 -- Sequential Progression II: Electruc Boogaloo
+2019-12-11
+*/
+
+
+var one = document.getElementById("one");
+one.addEventListener("click", factEx);
+function factEx(){
+  console.log(fibonacci(10));
+}
+
+var two = document.getElementById("two");
+two.addEventListener("click", gcdEx);
+function gcdEx(){
+  console.log(gcd(88,56));
+}
+
+var three = document.getElementById("three");
+three.addEventListener("click", randomStudentEx);
+function randomStudentEx(){
+  console.log(randomStudent());
 }
